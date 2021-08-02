af934fe97c31e4a3752a87b141b3b8826ad9412c
404NotYukariFound <1224615301@qq.com>
2021-08-02 11:33:23 +0800

    再次更新DateUtil文件

--- src/main/java/com/eight/utils/DateUtil.java
+++ src/main/java/com/eight/utils/DateUtil.java
@@ -13,13 +13,13 @@ public class DateUtil {
     /**
      * 把日期格式转换成年月日(yyyy-MM-dd)的字符串
      */
-    public static String dateConvertString(Date date) {
+    public static String dateConvertString(Date date){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.format(date);
     }
 
-    public static Timestamp dataConvertTimeStamp(String date) {
-        if (date.split(":").length != 3) {
+    public static Timestamp dataConvertTimeStamp(String date){
+        if(date.split(":").length != 3){
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             try {
                 return new Timestamp(sdf.parse(date).getTime());
@@ -30,14 +30,14 @@ public class DateUtil {
         return Timestamp.valueOf(date);
     }
 
-    public static Timestamp timestamp8HBefor(Timestamp timestamp) {
+    public static Timestamp timestamp8HBefor(Timestamp timestamp){
         Date date = new Date(timestamp.getTime());
         date.setTime(date.getTime() - 60 * 60 * 8 * 1000);
 
         return new Timestamp(date.getTime());
     }
 
-    public static Timestamp timestamp1DBefor(Timestamp timestamp) {
+    public static Timestamp timestamp1DBefor(Timestamp timestamp){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date(timestamp.getTime() - 60 * 60 * 24 * 1000);
         return dataConvertTimeStamp(sdf.format(date));
@@ -51,4 +51,3 @@ public class DateUtil {
     }
 
 }
-
